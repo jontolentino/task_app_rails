cf7b7df9732d1c9545c3c64e1a1c76e22114c1c5
Jon Tolentino <jontolentino123@gmail.com>
2021-07-09 20:54:39 +0800

    added seed date to categories and tasks

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+    has_many :tasks
+
+    before_create :slugify
+    def slugify
+        self.slug = name.parameterize
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([
+    {
+        name: "All"
+    },
+    {
+        name: "School"
+    },
+    {
+        name: "Work"
+    },
+    {
+        name: "Avionschool"
+    },
+    {
+        name: "Sidelines"
+    },
+    {
+        name: "Grocery"
+    }
+])
+
+tasks = Task.create([
+    {
+        name: "Math algebra assignment",
+        category: categories.second
+    },
+    {
+        name: "Math exercise at page 12",
+        category: categories.second
+    },
+    {
+        name: "Buy carrots 12 pieces",
+        category: categories.second
+    },
+    {
+        name: "Buy 1/2 kilo brown sugar",
+        category: categories.second
+    },
+    {
+        name: "add candy to grocery jar",
+        category: categories.second
+    },
+    {
+        name: "ruby journal project",
+        category: categories.second
+    },
+    {
+        name: "deploy rails to heroku",
+        category: categories.second
+    },
+    {
+        name: "add users to task app",
+        category: categories.second
+    }
+])
